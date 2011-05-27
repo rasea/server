0e4ea86b90e16fa656d2a0a1f229458d5698e6b7
Wilson Guimarães <wegneto@gmail.com>
2011-05-27 17:06:13 -0300

    Solução do Ticket #73: Independência do Servidor Rasea em relação ao Agente.

---
+++ src/main/java/org/rasea/core/security/PermissionCache.java
@@ -0,0 +1,51 @@
+package org.rasea.core.security;
+
+public class PermissionCache {
+
+	private final String resource;
+
+	private final String operation;
+
+	public PermissionCache(String resource, String operation) {
+		this.resource = resource;
+		this.operation = operation;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((operation == null) ? 0 : operation.hashCode());
+		result = prime * result + ((resource == null) ? 0 : resource.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof PermissionCache)) {
+			return false;
+		}
+		PermissionCache other = (PermissionCache) obj;
+		if (operation == null) {
+			if (other.operation != null) {
+				return false;
+			}
+		} else if (!operation.equals(other.operation)) {
+			return false;
+		}
+		if (resource == null) {
+			if (other.resource != null) {
+				return false;
+			}
+		} else if (!resource.equals(other.resource)) {
+			return false;
+		}
+		return true;
+	}
+}
--- src/main/java/org/rasea/core/security/RaseaPermissionResolver.java
+++ src/main/java/org/rasea/core/security/RaseaPermissionResolver.java
@@ -2,19 +2,80 @@ package org.rasea.core.security;
 
 import static org.jboss.seam.ScopeType.SESSION;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Observer;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
+import org.jboss.seam.security.Credentials;
 import org.jboss.seam.security.permission.PermissionResolver;
+import org.rasea.core.entity.Application;
+import org.rasea.core.entity.Permission;
+import org.rasea.core.exception.RequiredException;
+import org.rasea.core.exception.StoreException;
+import org.rasea.core.service.ApplicationService;
+import org.rasea.core.service.PermissionService;
+import org.rasea.core.service.UserService;
+import org.rasea.extensions.entity.User;
 
 @Name("raseaPermissionStore")
 @Scope(SESSION)
 public class RaseaPermissionResolver implements PermissionResolver {
 
+	@In
+	private PermissionService permissionService;
+
+	@In
+	private ApplicationService applicationService;
+
+	@In
+	private UserService userService;
+
+	@In
+	private Credentials credentials;
+
+	@Logger
+	private Log log;
+
+	private List<PermissionCache> cache = new ArrayList<PermissionCache>();
+
+	private Application application;
+
+	@Observer("org.jboss.seam.security.loginSuccessful")
+	public void load() {
+		try {
+			application = applicationService.find("rasea-server");
+			User user = userService.load(credentials.getUsername());
+
+			List<Permission> permisssions = permissionService.find(application, user);
+
+			String resource, operation;
+			for (Permission permission : permisssions) {
+				resource = permission.getResource().getName();
+				operation = permission.getOperation().getName();
+
+				cache.add(new PermissionCache(resource, operation));
+				log.info("application #0: allow operation #1 on #2 to #3", application.getName(), permission
+						.getOperation().getName(), permission.getResource().getName(), user.getName());
+			}
+
+		} catch (RequiredException cause) {
+			throw new RuntimeException(cause);
+
+		} catch (StoreException cause) {
+			throw new RuntimeException(cause);
+		}
+	}
+
 	@Override
-	public boolean hasPermission(Object target, String action) {
-		return true;
+	public boolean hasPermission(Object resource, String operation) {
+		PermissionCache permission = new PermissionCache(resource.toString(), operation);
+		return cache.contains(permission);
 	}
 
 	@Override
