e1815df1e93141c46e12ec855824317caab5b7cc
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2011-06-09 20:38:26 +0800

    Criado método toString() para bean User.
    Change-Id: I1f0a4f49719290a66f17ecfbb101fa956885602d

--- src/main/java/org/rasea/core/domain/User.java
+++ src/main/java/org/rasea/core/domain/User.java
@@ -84,4 +84,11 @@ public class User implements Serializable {
 	public void setActivation(Date activation) {
 		this.activation = activation;
 	}
+
+	@Override
+	public String toString() {
+		return "User [login=" + login + ", name=" + name + ", email=" + email + ", password=" + password
+				+ ", creation=" + creation + ", activation=" + activation + "]";
+	}
+	
 }
