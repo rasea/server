7ac7b25b0042bfb7274e40427714d7df373e8ce0
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2011-06-09 10:34:25 -0300

    Ciração dos casos de teste e integração do UserManager com a fábrica de SimpleDB

--- src/main/java/org/rasea/core/domain/User.java
+++ src/main/java/org/rasea/core/domain/User.java
@@ -3,17 +3,6 @@ package org.rasea.core.domain;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.Index;
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.Length;
-
-@Entity
-@Table(name = "RASEA_USER")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = -5630651623043896485L;
@@ -21,50 +10,38 @@ public class User implements Serializable {
 	/**
 	 * Identificação do usuário representada pelo seu username (login).
 	 */
-	@Id
-	@Length(max = 255)
-	@Column(nullable = false, insertable = true)
 	private String login;
 
 	/**
 	 * Nome completo do usuário para exibição.
 	 */
-	@Length(max = 255)
-	@Column(nullable = true, insertable = false)
 	private String name;
 
 	/**
-	 * Endereço eletrônico do usuário. Não é permitido o cadastro de e-mails duplicados.
+	 * Endereço eletrônico do usuário. Não é permitido o cadastro de e-mails
+	 * duplicados.
 	 */
-	@Email
-	@Length(max = 255)
-	@Column(nullable = false, unique = true, insertable = true)
-	@Index(name = "idx_user_email")
 	private String email;
 
 	/**
 	 * Senha do usuário para acessar sua aplicação e o Rasea.
 	 */
-	@Length(max = 100)
-	@Column(nullable = false, insertable = true, updatable = false)
 	private String password;
 
 	/**
 	 * Data de criação da conta.
 	 */
-	@Column(nullable = false, insertable = true, updatable = false)
 	private Date creation;
 
 	/**
 	 * Data de ativação da conta.
 	 */
-	@Column(nullable = true, insertable = false, updatable = false)
 	private Date activation;
 
 	public String getLogin() {
 		return login;
 	}
-	
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
@@ -108,10 +85,10 @@ public class User implements Serializable {
 	public void setActivation(Date activation) {
 		this.activation = activation;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "User [login=" + login + ", name=" + name + ", email=" + email + ", password=" + password
-				+ ", creation=" + creation + ", activation=" + activation + "]";
+		return "User [login=" + login + ", name=" + name + ", email=" + email + ", password=" + password + ", creation=" + creation + ", activation="
+				+ activation + "]";
 	}
 }
--- src/main/java/org/rasea/core/manager/UserManager.java
+++ src/main/java/org/rasea/core/manager/UserManager.java
@@ -5,11 +5,11 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.rasea.core.domain.User;
 
-import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.simpledb.AmazonSimpleDB;
-import com.amazonaws.services.simpledb.AmazonSimpleDBClient;
 import com.amazonaws.services.simpledb.model.Attribute;
 import com.amazonaws.services.simpledb.model.CreateDomainRequest;
 import com.amazonaws.services.simpledb.model.GetAttributesRequest;
@@ -20,46 +20,37 @@ import com.amazonaws.services.simpledb.model.ReplaceableAttribute;
 import com.amazonaws.services.simpledb.model.SelectRequest;
 import com.amazonaws.services.simpledb.model.SelectResult;
 
-import br.gov.frameworkdemoiselle.transaction.Transactional;
-
 public class UserManager implements Serializable {
 
 	private static final long serialVersionUID = 2097626878174583664L;
 
-	// TODO: injetar aqui o carinha!
-	//@Inject
+	@Inject
 	private AmazonSimpleDB sdb;
-	
+
 	// TODO: isso aqui poderia ser definido em uma anotação na classe bean, ex: @Domain("Users")
 	public final String DOMAIN = "Users";
-	
+
 	public UserManager() throws IOException {
-		
-		// TODO: isso aqui alguma fábrica deverá prover e manter em escopo de aplicação
-		sdb = new AmazonSimpleDBClient(new PropertiesCredentials(
-				UserManager.class.getResourceAsStream("AwsCredentials.properties")));
-		
 		// TODO: isso aqui teria que ficar em algum inicializador para todos os domínios da aplicação
-        sdb.createDomain(new CreateDomainRequest(DOMAIN));
+		sdb.createDomain(new CreateDomainRequest(DOMAIN));
 	}
 
 	/**
-	 * Cria a conta do usuário persistindo o id, name, email, password e activation que já estão preenchidos no objeto
-	 * passado por parâmetro.
+	 * Cria a conta do usuário persistindo o id, name, email, password e
+	 * activation que já estão preenchidos no objeto passado por parâmetro.
 	 * 
 	 * @param user
 	 */
-	@Transactional
 	public void createAccount(User user) {
-		
-        List<ReplaceableAttribute> attrs = new ArrayList<ReplaceableAttribute>();
-        attrs.add(new ReplaceableAttribute("name", user.getName(), true));
-        attrs.add(new ReplaceableAttribute("password", user.getPassword(), true));
-        
-        // TODO: fazer para o restante dos campos do bean User 
-        // ...
-        
-        sdb.putAttributes(new PutAttributesRequest(DOMAIN, user.getLogin(), attrs));
+
+		List<ReplaceableAttribute> attrs = new ArrayList<ReplaceableAttribute>();
+		attrs.add(new ReplaceableAttribute("name", user.getName(), true));
+		attrs.add(new ReplaceableAttribute("password", user.getPassword(), true));
+
+		// TODO: fazer para o restante dos campos do bean User 
+		// ...
+
+		sdb.putAttributes(new PutAttributesRequest(DOMAIN, user.getLogin(), attrs));
 	}
 
 	/**
@@ -68,21 +59,20 @@ public class UserManager implements Serializable {
 	 * @param login
 	 * @return User
 	 */
-	@Transactional
 	public User findByLogin(String login) {
 		User user = null;
-		
+
 		GetAttributesResult result = sdb.getAttributes(new GetAttributesRequest(DOMAIN, login));
 		if (result != null) {
 			user = new User();
 			user.setLogin(login);
-			
+
 			List<Attribute> attributes = result.getAttributes();
 			if (attributes != null && !attributes.isEmpty()) {
 				for (Attribute attr : attributes) {
 					final String name = attr.getName();
 					final String value = attr.getValue();
-					
+
 					if ("name".equals(name)) {
 						user.setName(value);
 					} else if ("password".equals(name)) {
@@ -91,67 +81,69 @@ public class UserManager implements Serializable {
 				}
 			}
 		}
-		
+
 		return user;
 	}
-	
+
 	/**
 	 * Retorna o usuário com base no seu e-mail.
 	 * 
 	 * @param email
 	 * @return
 	 */
-	@Transactional
 	public User findByEmail(String email) {
 		User user = null;
-		
-        final String expr = "select * from `" + DOMAIN + "` where email = '" + email + "'";
-        
-        SelectRequest request = new SelectRequest(expr);
-        SelectResult result = sdb.select(request);
-        
-        if (result != null) {
+
+		final String expr = "select * from `" + DOMAIN + "` where email = '" + email + "'";
+
+		SelectRequest request = new SelectRequest(expr);
+		SelectResult result = sdb.select(request);
+
+		if (result != null) {
 			user = new User();
-        	
-            for (Item item : result.getItems()) {
-            	user.setLogin(item.getName());
-            	
-                for (Attribute attr : item.getAttributes()) {
+
+			for (Item item : result.getItems()) {
+				user.setLogin(item.getName());
+
+				for (Attribute attr : item.getAttributes()) {
 					final String name = attr.getName();
 					final String value = attr.getValue();
-					
+
 					if ("name".equals(name)) {
 						user.setName(value);
 					} else if ("password".equals(name)) {
 						user.setPassword(value);
 					}
-                }
-                
-                // FIXME: em princípio o campo "email" será único no domínio...
-                break;
-            }
-        }
-        
-        return user;
+				}
+
+				// FIXME: em princípio o campo "email" será único no domínio...
+				break;
+			}
+		}
+
+		return user;
 	}
 
 	public static void main(String[] args) throws Exception {
 		UserManager manager = new UserManager();
-		
+
 		User user = new User();
 		user.setLogin("john");
 		user.setName("John Doe");
 		user.setEmail("john@doe.com");
 		user.setPassword("pass");
-		
+
 		System.out.println("Creating account: " + user);
 		manager.createAccount(user);
-		
+
 		User user2 = manager.findByLogin("john");
 		System.out.println("Found by login: " + user2);
-		
+
 		User user3 = manager.findByEmail("john@doe.com");
 		System.out.println("Found by email: " + user3);
 	}
-	
+
+	public void deleteAccount(User user) {
+
+	}
 }
