def98374d93cf28b4b3d92a4e20c2d80f5cb0af1
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2011-06-13 09:34:33 -0400

    Ajustado bean Account.

--- src/main/java/org/rasea/core/domain/Account.java
+++ src/main/java/org/rasea/core/domain/Account.java
@@ -82,62 +82,11 @@ public class Account implements Serializable {
 		this.creationDate = creationDate;
 	}
 
-	// @Attribute(name = "cre_date")
-	// private Date creationDate;
-	//
-	// @Attribute(name = "act_date")
-	// private Date activationDate;
-	//
-	// /**
-	// * Código para ativação da conta.
-	// */
-	// @Attribute(name = "act_code")
-	// private String activationCode;
-
-	// public String getLogin() {
-	// return login;
-	// }
-	//
-	// public void setLogin(String login) {
-	// this.login = login;
-	// }
-	//
-	// public String getPassword() {
-	// return password;
-	// }
-	//
-	// public void setPassword(String password) {
-	// this.password = password;
-	// }
-	//
-	// public Date getCreationDate() {
-	// return creationDate;
-	// }
-	//
-	// public void setCreationDate(Date creationDate) {
-	// this.creationDate = creationDate;
-	// }
-	//
-	// public Date getActivationDate() {
-	// return activationDate;
-	// }
-	//
-	// public void setActivationDate(Date activationDate) {
-	// this.activationDate = activationDate;
-	// }
-	//
-	// public String getActivationCode() {
-	// return activationCode;
-	// }
-	//
-	// public void setActivationCode(String activationCode) {
-	// this.activationCode = activationCode;
-	// }
-	//
-	// @Override
-	// public String toString() {
-	// return "Account [login=" + login + ", password=" + password + ", creationDate=" + creationDate
-	// + ", activationDate=" + activationDate + ", activationCode=" + activationCode + "]";
-	// }
-
+	@Override
+	public String toString() {
+		return "Account [username=" + username + ", email=" + email + ", photoUrl=" + photoUrl + ", password="
+				+ password + ", creationDate=" + creationDate + ", activationCode=" + activationCode
+				+ ", activationDate=" + activationDate + "]";
+	}
+	
 }
